e1f0c997bb7c691555ff169cc851d0d2d54d905e
Til Schaller <schaller.til@gmail.com>
2025-01-16 18:58:39 +0100

    initial commit

---
+++ kernel/src/main.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <limine.h>
+
+// Halt and catch fire function.
+static void hcf(void) {
+    for (;;) {
+        asm ("hlt");
+    }
+}
+
+//Kernel entry point
+void kmain(void) {
+    // Ensure the bootloader actually understands our base revision (see spec).
+    if (LIMINE_BASE_REVISION_SUPPORTED == false) {
+        hcf();
+    }
+
+    // Ensure we got a framebuffer.
+    if (framebuffer_request.response == NULL
+     || framebuffer_request.response->framebuffer_count < 1) {
+        hcf();
+    }
+
+    // Fetch the first framebuffer.
+    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+
+    // Note: we assume the framebuffer model is RGB with 32-bit pixels.
+    for (size_t i = 0; i < 100; i++) {
+        volatile uint32_t *fb_ptr = framebuffer->address;
+        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+    }
+
+    // We're done, just hang...
+    hcf();
+}
