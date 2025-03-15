15921889c698a9c9b80044ef5903247a1d92bfc8
Til Schaller <tilschaller@t-online.de>
2025-03-15 20:08:52 +0100

    completed pmm_init

--- kernel/headers/mman.h
+++ kernel/headers/mman.h
@@ -1,6 +1,9 @@
 #ifndef __MMAN_H__
 #define __MMAN_H__
 
+#include <stddef.h>
+#include <stdint.h>
+
 #define PAGE_SIZE 0x200000 // huge pages only
 #define HIGHER_HALF 0xffff800000000000
 #define KERNEL_OFFSET 0xffffffff80000000
@@ -11,6 +14,22 @@
 #define PT_FLAG_NX 1 << 63
 #define PT_FLAG_2MB 1 << 7
 
+struct pm_chunk {
+  uintptr_t base;
+  size_t pages;
+  struct pm_chunk *next;
+  uint8_t bitmap[];
+};
+
+void pmm_set_page_used(uint8_t *bitmap_entry, int bit);
+void pmm_set_page_free(uint8_t *bitmap_entry, int bit);
+
+struct vm_chunk{
+  uintptr_t base;
+  size_t length;
+  uint64_t flags;
+  struct vm_chunk *next;
+};
 __attribute__((noreturn))
 void init_mman();
 
